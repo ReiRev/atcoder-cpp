66170502492f732756de3c8533eac20aabfaef5e
reirev <reirev2913@gmail.com>
2023-04-09 10:15:45 +0900

    AC

---
+++ abc296/A.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    string S;
+    cin >> S;
+
+    for (int i = 0; i < N - 1; i++)
+    {
+        if (S[i] == S[i + 1])
+        {
+            cout << "No" << endl;
+            return 0;
+        }
+    }
+    cout << "Yes" << endl;
+}
