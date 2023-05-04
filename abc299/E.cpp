86b2ba80d564199149d232920dcb8f23a8165f05
reirev <reirev2913@gmail.com>
2023-05-04 17:34:13 +0900

    AC

--- abc299/E.cpp
+++ abc299/E.cpp
@@ -6,156 +6,156 @@
 
 using namespace std;
 
-vector<vector<int>> get_min_distances(int s, const vector<vector<int>> &g)
+vector<int> get_min_distances(int s, const vector<vector<int>> &G)
 {
-    // distance, edge
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    vector<int> distances(g.size(), numeric_limits<int>::max());
-    pq.push(pair<int, int>(0, s));
-    distances[s] = 0;
+    vector<int> res(G.size(), -1);
 
-    while (!pq.empty())
+    queue<int> q;
+    q.push(s);
+    res[s] = 0;
+    while (!q.empty())
     {
-        int d = pq.top().first;
-        int from = pq.top().second;
-        pq.pop();
-
-        if (distances[from] < d)
+        int from = q.front();
+        q.pop();
+        for (int next : G[from])
         {
-            continue;
-        }
-
-        for (int to : g[from])
-        {
-            if (d + 1 < distances[to])
+            if (res[next] != -1)
             {
-                distances[to] = d + 1;
-                pq.push(pair<int, int>(d + 1, to));
+                continue;
             }
+            res[next] = res[from] + 1;
+            q.push(next);
         }
-    }
-
-    // 距離ごとに頂点を保存
-    vector<vector<int>> res(g.size() + 1);
-    for (size_t i = 0; i < distances.size(); i++)
-    {
-        res[distances[i]].push_back(i);
     }
     return res;
-}
-
-bool solve(vector<int> &colors, vector<pair<int, int>> pds, const vector<vector<vector<int>>> &edges_min_distances)
-{
-    if (pds.empty())
-    {
-        return true;
-    }
-
-    // 後ろから条件を満たすように解いていく。(pop_backが使えるので)
-    int e = pds.back().first;
-    int d = pds.back().second;
-    pds.pop_back();
-
-    // 距離d未満の部分は必ず白でないといけない。
-    for (int i = 0; i < d; i++)
-    {
-        for (int to_be_white : edges_min_distances[e][i])
-        {
-            if (colors[to_be_white] == 1)
-            {
-                return false;
-            }
-            colors[to_be_white] = 0;
-        }
-    }
-
-    // すでに距離dのうちのいずれかが黒になっているのなら、そのままで大丈夫
-    bool requirement_satisfied = false;
-    for (int to_be_black : edges_min_distances[e][d])
-    {
-        if (colors[to_be_black] == 1)
-        {
-            requirement_satisfied = true;
-            break;
-        }
-    }
-    if (requirement_satisfied)
-    {
-        return solve(colors, pds, edges_min_distances);
-    }
-
-    // まだ距離dのうちのいずれかが黒になっていない場合
-    // 距離dのものに対して、まだ未定のものを黒にして解けるか計算。
-    for (int to_be_black : edges_min_distances[e][d])
-    {
-        if (colors[to_be_black] == -1)
-        {
-            vector<int> tmp_colors(colors.size());
-            copy(colors.begin(), colors.end(), tmp_colors.begin());
-            tmp_colors[to_be_black] = 1;
-            bool solved = solve(tmp_colors, pds, edges_min_distances);
-            if (solved)
-            {
-                copy(tmp_colors.begin(), tmp_colors.end(), colors.begin());
-                return true;
-            }
-        }
-    }
-    return false;
 }
 
 int main()
 {
     int N, M;
     cin >> N >> M;
-    vector<vector<int>> g(N);
+
+    vector<vector<int>> G(N);
     for (int i = 0; i < M; i++)
     {
-        int x, y;
-        cin >> x >> y;
-        x--;
-        y--;
-        g[x].push_back(y);
-        g[y].push_back(x);
+        int u, v;
+        cin >> u >> v;
+        u--;
+        v--;
+        G[u].push_back(v);
+        G[v].push_back(u);
     }
 
     int K;
     cin >> K;
-    vector<pair<int, int>> pds(K);
-    for (pair<int, int> &pd : pds)
+    vector<pair<int, int>> pds;
+    for (int i = 0; i < K; i++)
     {
-        cin >> pd.first >> pd.second;
-        pd.first--;
+        int p, d;
+        cin >> p >> d;
+        p--;
+        pds.push_back(pair<int, int>(p, d));
     }
 
-    // ここの計算時間がかかりすぎて、間に合わない。
-    vector<vector<vector<int>>> edges_min_distances;
+    vector<vector<int>> min_distances;
     for (int i = 0; i < N; i++)
     {
-        edges_min_distances.push_back(get_min_distances(i, g));
+        min_distances.push_back(get_min_distances(i, G));
     }
 
+    // ある頂点から距離に応じて頂点を格納
+    // vs_distances[i][d]は、頂点iから距離dに位置する頂点の数
+    vector<vector<vector<int>>> vs_distances(N, vector<vector<int>>(G.size() + 1));
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            int d = min_distances[i][j];
+            vs_distances[i][d].push_back(j);
+        }
+    }
+
+    // -1: 未定、0: 白、1: 黒
     vector<int> colors(N, -1);
-    int solved = solve(colors, pds, edges_min_distances);
-    if (!solved)
+    // 白に塗らなければならないところは全て白にする
+    for (pair<int, int> key_val : pds)
+    {
+        int p = key_val.first;
+        int d = key_val.second;
+        for (int i = 0; i < d; i++)
+        {
+            for (int v : vs_distances[p][i])
+            {
+                colors[v] = 0;
+            }
+        }
+    }
+    for (int &c : colors)
+    {
+        if (c == -1)
+        {
+            c = 1;
+        }
+    }
+    // 条件を満たしているかチェック
+    bool ok = true;
+
+    // 1個以上黒があるかどうか
+    int n_black = 0;
+    for (int c : colors)
+    {
+        if (c == 1)
+        {
+            n_black++;
+        }
+    }
+    if (n_black == 0)
+    {
+        ok = false;
+    }
+
+    // 距離の問題
+    for (pair<int, int> key_val : pds)
+    {
+        int p = key_val.first;
+        int d = key_val.second;
+        for (int i = 0; i < d; i++)
+        {
+            for (int v : vs_distances[p][i])
+            {
+                if (colors[v] == 1)
+                {
+                    ok = false;
+                }
+            }
+        }
+        bool tmp_ok = false;
+        for (int v : vs_distances[p][d])
+        {
+            if (colors[v] == 1)
+            {
+                tmp_ok = true;
+            }
+        }
+        if (!tmp_ok)
+        {
+            ok = false;
+        }
+    }
+
+    if (ok)
+    {
+        cout << "Yes" << endl;
+        for (int c : colors)
+        {
+            cout << c;
+        }
+        cout << endl;
+    }
+    else
     {
         cout << "No" << endl;
     }
 
-    if (solved)
-    {
-        cout << "Yes" << endl;
-        for (int x : colors)
-        {
-            if (x == -1)
-            {
-                cout << 1;
-            }
-            else
-            {
-                cout << x;
-            }
-        }
-        cout << endl;
-    }
+    return 0;
 }
